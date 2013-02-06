639a11864b9a3ffee68364a7c7ed2d78fd259555
Jonatan Heyman <jonatan@heyman.info>
2013-02-06 17:01:17 +0100

    Initial import of the first version

---
+++ README.rst
@@ -0,0 +1,39 @@
+jQuery Draggable Touch
+======================
+
+Make HTML elements draggable. Main implementation uses touch events, 
+but the plugin also has a fallback that uses mouse events.
+
+The main reason that this plugin exist is that there are currently no 
+good jQuery plugin for making elements draggable, that has touchdevices 
+as it's main target (at least that I know of. jQuery UI (http://jqueryui.com/draggable/) 
+has a draggable plugin, that together with jQuery UI Touch Punch (http://touchpunch.furf.com/) 
+can be used to make elements draggable on touch devices. However, due to 
+Touch Punch generating fake mouse events, and jQuery UI's draggable plugin, using these fake 
+mouse events when dragging elements, it's error prone and contains wierd bugs.
+
+I decided it was simpler to just write a draggable plugin whose main target 
+was touch devices, and that uses touch events (even though it still has a 
+fallback on mouseevents when the browser/device doesn't support touch events).
+
+Usage example
+-------------
+
+::
+
+    $(".my-draggables")
+        .draggableTouch()
+        .bind("dragstart", function(event, pos) {
+            console.log("drag started on:", this, "at position:", pos);
+        })
+        .bind("dragend", function(event, pos) {
+            console.log("drag ended on:", this, "at position:", post);
+        });
+
+Copyright & License
+-------------------
+
+This plugin is written by `Jonatan Heyman <http://heyman.info>`_ and is licenced as 
+`Beerware <http://en.wikipedia.org/wiki/Beerware>`_.
+
+
---
+++ jquery.draggableTouch.js
@@ -0,0 +1,91 @@
+/**
+ * jQuery DraggableTouch v0.5
+ *
+ * Jonatan Heyman | http://heyman.info 
+ *
+ * Make HTML elements draggable. Main implementation uses touch events, 
+ * but the plugin also has a fallback that uses mouse events.
+ *
+ */
+;(function($){
+    $.fn.draggableTouch = function() {
+        // check if the device has touch support, and if not, fallback to use mouse
+        // draggableMouse which uses mouse events
+        if (!("ontouchstart" in document.documentElement)) {
+            this.draggableMouse();
+            return;
+        }
+        this.each(function() {
+            var element = $(this);
+            var offset = null;
+            
+            var end = function(e) {
+                e.preventDefault();
+                var orig = e.originalEvent;
+                
+                element.trigger("dragend", {
+                    top: orig.changedTouches[0].pageY - offset.y,
+                    left: orig.changedTouches[0].pageX - offset.x
+                });
+            };
+            
+            element.bind("touchstart", function(e) {
+                var orig = e.originalEvent;
+                var pos = $(this).position();
+                offset = {
+                    x: orig.changedTouches[0].pageX - pos.left,
+                    y: orig.changedTouches[0].pageY - pos.top
+                };
+                element.trigger("dragstart", pos);
+            });
+            element.bind("touchmove", function(e) {
+                e.preventDefault();
+                var orig = e.originalEvent;
+                
+                // do now allow two touch points to drag the same element
+                if (orig.targetTouches.length > 1)
+                    return;
+                
+                $(this).css({
+                    top: orig.changedTouches[0].pageY - offset.y,
+                    left: orig.changedTouches[0].pageX - offset.x
+                });
+            });
+            element.bind("touchend", end);
+            element.bind("touchcancel", end);
+        });
+    };
+    
+    /**
+     * Draggable fallback for when touch is not available
+     */
+    $.fn.draggableMouse = function () {
+        this.each(function() {
+            var element = $(this);
+            var offset = null;
+            
+            var move = function(e) {
+                element.css({
+                    top: e.pageY - offset.y,
+                    left: e.pageX - offset.x,
+                });
+            };
+            element.bind("mousedown", function(e) {
+                var pos = element.position();
+                offset = {
+                    x: e.pageX - pos.left,
+                    y: e.pageY - pos.top
+                };
+                $(document).bind("mousemove", move);
+                element.trigger("dragstart", pos);
+            });
+            element.bind("mouseup", function(e) {
+                $(document).unbind("mousemove", move);
+                element.trigger("dragend", {
+                    top: e.pageY - offset.y,
+                    left: e.pageX - offset.x
+                });
+            });
+        });
+    };
+})(jQuery);
